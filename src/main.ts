ca0cfeeb308c9d81eaf980431143f7d5c627045a
kingmiclong <b302057069@gmail.com>
2023-10-18 09:40:16 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,9 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+// Add the canvas element
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+app.appendChild(canvas);
