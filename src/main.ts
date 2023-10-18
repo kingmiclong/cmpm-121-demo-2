77c25d40856ca886eef6da5299de45f0d7443142
kingmiclong <b302057069@gmail.com>
2023-10-18 13:54:41 -0700

    Stage 6 complete~

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,8 @@ const ctx = canvas.getContext("2d")!;
 // Variables for storing drawing state
 let drawing = false;
 let currentStroke: { x: number; y: number }[] = [];
-let allStrokes: { x: number; y: number }[][] = [];
+// let allStrokes: { x: number; y: number }[][] = [];
+let currentThickness = 1;
 
 // Custom event to notify changes in drawing
 const drawEvent = new Event("drawing-changed");
@@ -60,19 +61,20 @@ canvas.addEventListener("mousemove", (event) => {
 // Update mouse up event to push stroke to undoStack
 canvas.addEventListener("mouseup", () => {
   drawing = false;
-  const newCommand = new MarkerLineCommand(currentStroke[0]);
+  const newCommand = new MarkerLineCommand(currentStroke[0], currentThickness);
   currentStroke.slice(1).forEach((point) => newCommand.drag(point.x, point.y));
   undoStack.push(newCommand);
-  allStrokes.push(currentStroke);
   canvas.dispatchEvent(drawEvent);
 });
 
 // Command class for Marker Line
 class MarkerLineCommand {
   private points: { x: number; y: number }[] = [];
+  private thickness: number;
 
-  constructor(initialPoint: { x: number; y: number }) {
+  constructor(initialPoint: { x: number; y: number }, thickness: number) {
     this.points.push(initialPoint);
+    this.thickness = thickness;
   }
 
   // Method to add a point to the line
@@ -83,6 +85,7 @@ class MarkerLineCommand {
   // Method to display the line on the canvas
   display(ctx: CanvasRenderingContext2D) {
     ctx.beginPath();
+    ctx.lineWidth = this.thickness; // Set line thickness
     ctx.moveTo(this.points[0].x, this.points[0].y);
     this.points.forEach((point) => {
       ctx.lineTo(point.x, point.y);
@@ -99,8 +102,9 @@ const redoStack: MarkerLineCommand[] = [];
 const clearButton = document.createElement("button");
 clearButton.innerHTML = "Clear";
 clearButton.addEventListener("click", () => {
-  allStrokes = [];
-  canvas.dispatchEvent(drawEvent); // Clear canvas
+  undoStack.length = 0;
+  redoStack.length = 0;
+  canvas.dispatchEvent(drawEvent); // Redraw the canvas
 });
 
 app.appendChild(clearButton);
@@ -129,3 +133,23 @@ redoButton.addEventListener("click", () => {
 
 app.appendChild(undoButton);
 app.appendChild(redoButton);
+
+// Create and append Thin button
+const thinButton = document.createElement("button");
+thinButton.innerHTML = "Thin";
+thinButton.addEventListener("click", () => {
+  currentThickness = 1;
+  thinButton.classList.add("selectedTool"); // Optional: Add CSS class for visual feedback
+  thickButton.classList.remove("selectedTool");
+});
+app.appendChild(thinButton);
+
+// Create and append Thick button
+const thickButton = document.createElement("button");
+thickButton.innerHTML = "Thick";
+thickButton.addEventListener("click", () => {
+  currentThickness = 5;
+  thickButton.classList.add("selectedTool"); // Optional: Add CSS class for visual feedback
+  thinButton.classList.remove("selectedTool");
+});
+app.appendChild(thickButton);
